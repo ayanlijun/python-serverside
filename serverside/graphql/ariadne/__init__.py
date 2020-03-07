5948d2ec1f737a5350c93f8e660923cd1bad451f
asciialex <alex@asciialex.com>
2020-03-07 12:02:53 +0800

    Exported base ariadne functions from the serverside library itself

--- serverside/graphql/ariadne/__init__.py
+++ serverside/graphql/ariadne/__init__.py
@@ -1,3 +1,4 @@
+from ariadne import ObjectType, QueryType, MutationType
 from .base_resolver import BaseResolver
 from .django_models import (
     django_get_one,
@@ -8,6 +9,9 @@ from .django_models import (
 )
 
 __all__ = [
+    "ObjectType",
+    "QueryType",
+    "MutationType",
     "BaseResolver",
     "django_get_one",
     "django_get_many",

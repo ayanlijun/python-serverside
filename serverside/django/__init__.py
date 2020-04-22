7670981efce4eca46276ed347a9fe6b8e5ae5c9e
asciialex <alex@asciialex.com>
2020-04-22 09:41:26 +0800

    Added a depth validator into Django utilities

--- serverside/django/__init__.py
+++ serverside/django/__init__.py
@@ -1,9 +1,11 @@
 from .apps import get_apps
 from .fields import S3FieldObject, S3Field
+from .validators import DepthValidator
 
 
 __all__ = [
     "get_apps",
     "S3FieldObject",
-    "S3Field"
+    "S3Field",
+    "DepthValidator"
 ]
